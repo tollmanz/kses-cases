60aad6f01d628fe0ed26259ec022f4b3a9239b52
Jonathan Desrosiers <desrosj@gmail.com>
2019-08-19 16:12:32 -0400

    Fix copy/paste error.

--- kses.php
+++ kses.php
@@ -1830,23 +1830,6 @@ function wp_check_invalid_utf8( $string, $strip = false ) {
  * @since 1.2.2
  * @access private
  *
- * @param string $string The text which is to be encoded.
- * @param mixed $quote_style Optional. Converts double quotes if set to ENT_COMPAT, both single and double if set to ENT_QUOTES or none if set to ENT_NOQUOTES. Also compatible with old values; converting single quotes if set to 'single', double if set to 'double' or both if otherwise set. Default is ENT_NOQUOTES.
- * @param string $charset Optional. The character encoding of the string. Default is false.
- * @param boolean $double_encode Optional. Whether to encode existing html entities. Default is false.
- * @return string The encoded text with HTML entities.
- */
-function /**
- * Converts a number of special characters into their HTML entities.
- *
- * Specifically deals with: &, <, >, ", and '.
- *
- * $quote_style can be set to ENT_COMPAT to encode " to
- * &quot;, or ENT_QUOTES to do both. Default is ENT_NOQUOTES where no quotes are encoded.
- *
- * @since 1.2.2
- * @access private
- *
  * @staticvar string $_charset
  *
  * @param string       $string        The text which is to be encoded.
